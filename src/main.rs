099f6c66cfe1c9ce48f5ac386db172d71ba7425f
hotcocoancode <elee9@madison.k12.wi.us>
2024-11-24 00:44:59 -0600

    start moving towards having window decorations

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ use std::ffi::{c_char, c_int, c_uint, CStr};
 use std::process::exit;
 use std::{mem, ptr};
 use x11::xlib;
+use x11::xlib::XWindowChanges;
 
 fn close_quit(display: *mut xlib::Display, code: i32) {
     unsafe {
@@ -15,7 +16,7 @@ fn close_quit(display: *mut xlib::Display, code: i32) {
 
 unsafe extern "C" fn handle_wm_open(display: *mut xlib::Display, xerror: *mut xlib::XErrorEvent) -> c_int {
     if (*xerror).error_code == xlib::BadAccess {
-        eprintln!("WM already existed on Display {:?} :despair:",  CStr::from_ptr(xlib::XDisplayString(display)));
+        eprintln!("WM already existed on display {:?}",  CStr::from_ptr(xlib::XDisplayString(display)));
         exit(1);
     }
     0
@@ -42,11 +43,14 @@ fn main() {
 
     // Maybe an FFI project as my first real one was a bad plan...
     unsafe {
-        //xlib::XSetErrorHandler(Some(handle_wm_open));
-        //xlib::XSelectInput(display, root, xlib::SubstructureRedirectMask | xlib::SubstructureNotifyMask);
-        //::XSync(display, false as c_int);
+        xlib::XSetErrorHandler(Some(handle_wm_open));
+        xlib::XSelectInput(display, root, xlib::SubstructureRedirectMask | xlib::SubstructureNotifyMask);
+        xlib::XSync(display, false as c_int);
 
         xlib::XSetErrorHandler(Some(error));
+
+        // TODO: This should work in theory, but XQuartz doesn't like Alt + FN + FX key combo.
+        xlib::XGrabKey(display, xlib::XKeysymToKeycode(display, x11::keysym::XK_F4 as std::ffi::c_ulong) as c_int, xlib::Mod1Mask, root, false as c_int, xlib::GrabModeAsync, xlib::GrabModeAsync);
 
         xlib::XGrabButton(display, 1, xlib::Mod1Mask, root, 1, (xlib::ButtonPressMask|xlib::ButtonReleaseMask|xlib::PointerMotionMask) as c_uint, xlib::GrabModeAsync, xlib::GrabModeAsync, 0, 0);
         xlib::XGrabButton(display, 1, xlib::ShiftMask, root, 1, xlib::ButtonPressMask as c_uint, xlib::GrabModeAsync, xlib::GrabModeAsync, 0, 0);
@@ -56,21 +60,23 @@ fn main() {
     // Rust was a mistake Rust was a mistake Rust was a mistake
     let mut start_drag: Option<(c_int, c_int, c_uint, xlib::Window)> = None;
     let mut window_attributes: xlib::XWindowAttributes = unsafe { mem::zeroed() };
-    let mut top_window: xlib::Window = 0;
+    let mut focused_window: xlib::Window = 0;
     loop {
         let mut event: xlib::XEvent = unsafe { mem::zeroed() };
+        // Atp there is a comical amount of unsafe blocks.
+        // Is this even worth doing in Rust?
         unsafe { xlib::XNextEvent(display, &mut event); }
         match event.get_type() {
-            // Atp there is a comical amount of unsafe blocks.
-            // Is this even worth doing in Rust?
+            // All my mouse button besties
             xlib::ButtonPress => unsafe {
                 if event.button.subwindow != 0 {
                     if (event.button.state & xlib::Mod1Mask) == xlib::Mod1Mask {
                         xlib::XGetWindowAttributes(display, event.button.subwindow, &mut window_attributes);
                         start_drag = Option::from((event.button.x_root, event.button.y_root, event.button.button, event.button.subwindow));
-                    } else if top_window != event.button.subwindow { // Assumed state = ShiftMask
+                    }
+                    if focused_window != event.button.subwindow { // Assumed state = ShiftMask or Mod1Mask
                         xlib::XRaiseWindow(display, event.button.subwindow);
-                        top_window = event.button.subwindow;
+                        focused_window = event.button.subwindow;
                     }
                 }
             },
@@ -92,6 +98,24 @@ fn main() {
                     start_drag = None;
                 }
             },
+            xlib::KeyPress => unsafe { // Honorary mouse member (key)
+                xlib::XKillClient(display, focused_window);
+            },
+
+            // Okay, now time to manage the windows.
+            xlib::ConfigureRequest => unsafe {
+                let mut changes: XWindowChanges =mem::zeroed();
+                changes.x = event.configure_request.x;
+                changes.y = event.configure_request.y;
+                changes.width = event.configure_request.width;
+                changes.height = event.configure_request.height;
+                changes.border_width = event.configure_request.border_width;
+                changes.sibling = event.configure_request.above;
+                changes.stack_mode = event.configure_request.detail;
+
+                xlib::XConfigureWindow(display, event.configure_request.window, event.configure_request.value_mask as c_uint, &mut changes);
+
+            }
             _ => {}
         };
     }
